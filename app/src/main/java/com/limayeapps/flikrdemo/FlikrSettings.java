9a29827bbf5897ed6f620df0887314fb7c74f426
Kshitij Limaye <klimaye@gmail.com>
2015-05-23 11:32:37 -0500

    initial commit

---
+++ app/src/main/java/com/limayeapps/flikrdemo/FlikrSettings.java
@@ -0,0 +1,9 @@
+package com.limayeapps.flikrdemo;
+
+/**
+ * Created by kshitijlimaye on 5/23/15.
+ */
+public class FlikrSettings {
+    public static final String API_KEY = "";
+    public static final String SECRET = "";
+}
